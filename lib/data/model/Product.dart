4abdd8bcd11bfe31eb28b2992634c8ef553d1c07
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2023-03-16 20:28:52 +0200

    Inventory done TODO connect inv with DB

--- lib/data/model/Product.dart
+++ lib/data/model/Product.dart
@@ -1,14 +1,25 @@
 class Product {
+  int? id;
   String name;
   int quantity;
   int buyPrice;
   int sellPrice;
-  int id;
 
-  Product(
-      {required this.id,
-      required this.name,
-      required this.quantity,
-      required this.buyPrice,
-      required this.sellPrice});
+  Product({
+    required this.id,
+    required this.name,
+    required this.quantity,
+    required this.buyPrice,
+    required this.sellPrice,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'name': name,
+      'quantity': quantity,
+      'buyPrice': buyPrice,
+      'sellPrice': sellPrice,
+      'id': id,
+    };
+  }
 }
--- lib/screens/example/example.dart
+++ lib/screens/example/example.dart
@@ -1,262 +1,280 @@
-import 'dart:math';
-
-import 'package:cashier/data/local/database.dart';
-import 'package:cashier/data/model/Product.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-import 'package:flutter_screenutil/flutter_screenutil.dart';
-
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    final _scrollController = ScrollController();
-
-    List<Product> _items = [
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-    ];
-    List<String> emptyList = [
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-      "",
-    ];
-
-    List<Product> _items1 = [
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'كرتونه بيض احمر',
-          quantity: 1,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'طماطم حمرا',
-          quantity: 2,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-      Product(
-          name: 'فلفل احمر',
-          quantity: 3,
-          buyPrice: 12,
-          sellPrice: 14,
-          id: 1),
-    ];
-
-    return Scaffold(
-        
-      body: Column(
-        children: [
-          Container(
-            margin: EdgeInsets.all(50),
-            decoration: const BoxDecoration(
-              color: Color(0xFFffffff),
-              boxShadow: [
-                BoxShadow(
-                  color: Colors.grey,
-                  blurRadius: 15.0, // soften the shadow
-                  spreadRadius: 5.0, //extend the shadow
-                  offset: Offset(
-                    5.0, // Move to right 5  horizontally
-                    5.0, // Move to bottom 5 Vertically
-                  ),
-                )
-              ],
-            ),
-            child: Column(
-              children: [
-                Container(
-                  padding: EdgeInsets.all(10),
-                  child: Row(
-                    children: [
-                      Expanded(child: Text('Name')),
-                      SizedBox(width: 16),
-                      Text('Price', style: TextStyle(fontWeight: FontWeight.bold)),
-                      SizedBox(width: 16),
-                      Text('Quantity', style: TextStyle(fontWeight: FontWeight.bold)),
-                    ],
-                  ),
-                ),
-                Divider(thickness: 1),
-                Container(
-                  padding: EdgeInsets.all(10),
-                  height: 900.h,
-                  child: ListView.builder(
-                    shrinkWrap: true,
-                    itemCount: _items.length,
-                    itemBuilder: (BuildContext context, int index) {
-                      return Row(
-                        children: [
-                          Expanded(
-                            child: Text(_items[index].name),
-                          ),
-                          SizedBox(width: 16),
-                          Text(
-                            '\$${_items[index].sellPrice.toStringAsFixed(2)}',
-                            style: TextStyle(fontWeight: FontWeight.bold),
-                          ),
-                          SizedBox(width: 16),
-                          Row(
-                            mainAxisSize: MainAxisSize.min,
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.remove),
-                                onPressed: () => {
-
-                                },
-                              ),
-                              Text(_items[index].quantity.toString()),
-                              IconButton(
-                                icon: Icon(Icons.add),
-                                onPressed: () async  =>
-                                {
-
-                                  await SQLHelper.initDb(),
-                                  print(await SQLHelper.addInvoice(20 , _items)),
-
-                                  // SQLHelper.getproducts().then((value) => {
-                                  //
-                                  // for (Map<String, dynamic> pro in value) {
-                                  //     print(pro.toString())
-                                  // }
-                                  //
-                                  // }),
-                                }
-                              ),
-                            ],
-                          ),
-                        ],
-                      );
-                    },
-                  ),
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-      
-    );
-  }
-}
+// import 'dart:math';
+//
+// import 'package:cashier/data/model/Product.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+// import 'package:syncfusion_flutter_datagrid/datagrid.dart';
+//
+// class HomeScreen extends StatelessWidget {
+//   const HomeScreen({Key? key}) : super(key: key);
+//
+//   @override
+//   Widget build(BuildContext context) {
+//     final _scrollController = ScrollController();
+//
+//     //String? name;
+//     //   String? price;
+//     //   int? quantity;
+//     //   int? boxQuantity;
+//     //   int? buyPrice;
+//     //   int? sellPrice;
+//     //   int? id;
+//     List<Product> _items = [
+//       Product(
+//           name: 'كرتونه بيض احمر',
+//           quantity: 1,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'طماطم حمرا',
+//           quantity: 2,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'فلفل احمر',
+//           quantity: 3,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'كرتونه بيض احمر',
+//           quantity: 1,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'طماطم حمرا',
+//           quantity: 2,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'فلفل احمر',
+//           quantity: 3,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'كرتونه بيض احمر',
+//           quantity: 1,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'طماطم حمرا',
+//           quantity: 2,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'فلفل احمر',
+//           quantity: 3,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'كرتونه بيض احمر',
+//           quantity: 1,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'طماطم حمرا',
+//           quantity: 2,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//       Product(
+//           name: 'فلفل احمر',
+//           quantity: 3,
+//           boxQuantity: 10,
+//           buyPrice: 12,
+//           sellPrice: 14,
+//           id: 1),
+//     ];
+//     List<String> emptyList = [
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//       "",
+//     ];
+//
+//     List<Product> _items1 = [
+//       // Product(
+//       //     name: 'كرتونه بيض احمر',
+//       //     quantity: 1,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'طماطم حمرا',
+//       //     quantity: 2,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'فلفل احمر',
+//       //     quantity: 3,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'كرتونه بيض احمر',
+//       //     quantity: 1,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'طماطم حمرا',
+//       //     quantity: 2,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'فلفل احمر',
+//       //     quantity: 3,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'كرتونه بيض احمر',
+//       //     quantity: 1,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'طماطم حمرا',
+//       //     quantity: 2,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//       // Product(
+//       //     name: 'فلفل احمر',
+//       //     quantity: 3,
+//       //     boxQuantity: 10,
+//       //     buyPrice: 12,
+//       //     sellPrice: 14,
+//       //     id: 1),
+//     ];
+//
+//     DataSource dataSource = DataSource(_items);
+//
+//     return Scaffold(
+//         body: SfDataGrid(
+//           allowSorting: true,
+//           selectionMode: SelectionMode.multiple,
+//           source: dataSource,
+//           columns: [
+//             GridTextColumn(
+//                 columnName: 'id',
+//                 label: Container(
+//                   padding: EdgeInsets.symmetric(horizontal: 16.0),
+//                   alignment: Alignment.centerRight,
+//                   child: Text(
+//                     'ID',
+//                     overflow: TextOverflow.ellipsis,
+//                   ),
+//                 )),
+//             GridTextColumn(
+//                 columnName: 'name',
+//                 label: Container(
+//                     padding: EdgeInsets.symmetric(horizontal: 16.0),
+//                     alignment: Alignment.centerLeft,
+//                     child: Text(
+//                       'Name',
+//                       overflow: TextOverflow.ellipsis,
+//                     ))),
+//             GridTextColumn(
+//                 columnName: 'designation',
+//                 label: Container(
+//                     padding: EdgeInsets.symmetric(horizontal: 16.0),
+//                     alignment: Alignment.centerLeft,
+//                     child: Text(
+//                       'Designation',
+//                       overflow: TextOverflow.ellipsis,
+//                     ))),
+//             GridTextColumn(
+//                 columnName: 'salary',
+//                 label: Container(
+//                     padding: EdgeInsets.symmetric(horizontal: 16.0),
+//                     alignment: Alignment.centerRight,
+//                     child: Text(
+//                       'Salary',
+//                       overflow: TextOverflow.ellipsis,
+//                     ))),
+//           ],
+//         ),
+//
+//     );
+//   }
+//
+// }
+//
+//
+// class DataSource extends DataGridSource {
+//   DataSource(List<Product> products) {
+//     dataGridRows = products
+//         .map<DataGridRow>((dataGridRow) => DataGridRow(cells: [
+//       DataGridCell<int>(columnName: 'id', value: dataGridRow.id),
+//       DataGridCell<String>(columnName: 'name', value: dataGridRow.name),
+//       DataGridCell<String>(
+//           columnName: 'price', value: dataGridRow.sellPrice.toString()),
+//       DataGridCell<int>(
+//           columnName: 'quantity', value: dataGridRow.quantity),
+//     ]))
+//         .toList();
+//   }
+//
+//   late List<DataGridRow> dataGridRows;
+//   @override
+//   List<DataGridRow> get rows => dataGridRows;
+//   @override
+//   DataGridRowAdapter? buildRow(DataGridRow row) {
+//     return DataGridRowAdapter(
+//         cells: row.getCells().map<Widget>((dataGridCell) {
+//           return Container(
+//               padding: EdgeInsets.symmetric(horizontal: 16.0),
+//               alignment: (dataGridCell.columnName == 'id' ||
+//                   dataGridCell.columnName == 'salary')
+//                   ? Alignment.centerRight
+//                   : Alignment.centerLeft,
+//               child: Text(
+//                 dataGridCell.value.toString(),
+//                 overflow: TextOverflow.ellipsis,
+//               ));
+//         }).toList());
+//   }
+// }
